d2d52cb8cc5a03c39abfdcaf27152ea365fa8010
Henri Fournier <hfournier@intratela.com>
2023-09-22 11:49:40 -0700

    Updated for mobile

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        brand: "#ffcd00",
+        brand: "#F3D03E",
       },
       fontFamily: {
         calibri: ["Calibri", "sans-serif"],
